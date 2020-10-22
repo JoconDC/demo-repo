c27ca53ab5ddd3596fafccb3eb8952b85f6d871d
Jojo <57081401+JoconDC@users.noreply.github.com>
2020-10-21 20:43:02 -0400

    Added files to demo-repo

---
+++ app.js
@@ -0,0 +1,38 @@
+
+/*
+var john = {
+    name: 'John',
+    yearOfBirth: 1998,
+    job: 'teacher'
+} */
+
+/*
+var Person = function(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+    this.calculateAge = function() {console.log(2020 - this.yearOfBirth)
+    }
+}
+
+Person.prototype.calculateAge =
+    function() {console.log(2020 - this.yearOfBirth)
+    }
+
+  
+
+
+var john = new Person('John', 1998, 'teacher'); 
+var jane = new Person('Jane', 1969, 'designer'); 
+var mark = new Person('Mark', 1948, 'police'); 
+var jocon = new Person('Jocon', 1973, 'software');
+
+
+
+
+john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
+jocon.calculateAge(); 
+*/
+
